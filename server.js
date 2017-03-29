8f36473f7c0c37ea4d1fe152e46991c3f62112c1
Rhannyuy <rhannyuy@13-MacBook-Air-E-14.local>
2017-03-29 14:29:45 -0600

    Changes

--- server.js
+++ server.js
@@ -1,28 +1,43 @@
+
+var http = require("http");
 var fs = require("fs");
-var http = require('http');
-var port = 8080;
 
+var pageCount = parseInt(fs.readFileSync('./count.txt'));
 
-function requesthandler(req, res){
-	console.log(req.url)
-	
-
-	if(req.url === "/"){
-		fs.readFile('count.txt', function(err, data){
-			if (err){
-				console.log(err);
-			} else {
-				res.end(data)
-			}
-		});
-		
-	}
+function updateCount(fileName, pageCount){
+    var newvalue = pageCount;
+    fs.writeFile(fileName, newvalue, function(err){
+    //fs.writeFile(filename, newcontents, callback)`
+      if(err){
+        console.log(err);
+        return
+      }
+    });
 }
 
+//
+
+var server = http.createServer(function(req, res){
+  if(req.url === "/") { //local host 8080, homepge
+    fs.readFile("./pets.html", function(err, data){
+      res.write(data);
+      pageCount = pageCount + 1;
+      updateCount('./count.txt', pageCount);
+      res.end();
+      //impliment some more stuff
+    });
+  } else if (req.url === '/count'){ //count page
+    fs.readFile("./count.txt", function(err, data){
+      res.write(data);
+      res.end();
+      //some stuff about times site visited
+    });
+  }
+});
 
 
-var server = http.createServer(requesthandler)
+//listen for new connections on this port
+//must be between 1025 - 65535 (all lower than 1025 are administrative. Don't run )
+server.listen(8080);
 
-server.listen(port, function(){
-	console.log('listening on', port)
-})
+console.log("working");
